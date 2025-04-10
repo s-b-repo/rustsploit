d1580a87179d00d0f3b77dea1367c0cddb6de325
S.B <30941141+s-b-repo@users.noreply.github.com>
2025-04-10 18:18:26 +0200

    Update creds.rs

--- src/commands/creds.rs
+++ src/commands/creds.rs
@@ -6,8 +6,17 @@ pub async fn run_cred_check(module_name: &str, target: &str) -> Result<()> {
         "sample_cred_check" => {
             creds::sample_cred_check::run(target).await?;
         },
+        "ftp_bruteforce" => {
+        creds::ftp_bruteforce::run(target).await?;
+        },
+        "ftp_anonymous" => {
+            creds::ftp_anonymous::run(target).await?;
+        },
         // Add more creds modules here ...
-        _ => eprintln!("Creds module '{}' not found.", module_name),
+        _ => {
+            eprintln!("Creds module '{}' not found.", module_name);
+        },
     }
+
     Ok(())
 }
