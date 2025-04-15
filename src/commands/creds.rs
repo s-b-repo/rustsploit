ac3852382313fe9aa50f6d2e5be63085c4231054
S.B <30941141+s-b-repo@users.noreply.github.com>
2025-04-15 09:37:21 +0200

    Add files via upload

---
+++ src/commands/creds.rs
@@ -0,0 +1,22 @@
+use anyhow::{Result, bail};
+
+use crate::modules::creds::generic::{
+    ftp_anonymous,
+    ftp_bruteforce,
+    sample_cred_check,
+    telnet_bruteforce,
+    ssh_bruteforce,
+};
+
+pub async fn run_cred_check(module_name: &str, target: &str) -> Result<()> {
+    match module_name {
+        "generic/sample_cred_check"     => sample_cred_check::run(target).await?,
+        "generic/ftp_bruteforce"        => ftp_bruteforce::run(target).await?,
+        "generic/ftp_anonymous"         => ftp_anonymous::run(target).await?,
+        "generic/telnet_bruteforce"     => telnet_bruteforce::run(target).await?,
+        "generic/ssh_bruteforce"        => ssh_bruteforce::run(target).await?,
+        _ => bail!("Creds module '{}' not found.", module_name),
+    }
+
+    Ok(())
+}
---
+++ src/modules/creds/generic/mod.rs
@@ -0,0 +1,7 @@
+
+    pub mod sample_cred_check;
+    pub mod ftp_bruteforce;
+    pub mod ftp_anonymous;
+    pub mod telnet_bruteforce;
+    pub mod ssh_bruteforce;
+
