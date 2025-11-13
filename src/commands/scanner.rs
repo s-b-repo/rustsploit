de0ed82830a7fca4b2d60fdbe69c74f1cef869b1
S.B <30941141+s-b-repo@users.noreply.github.com>
2025-11-13 09:37:40 +0200

    Delete src directory

--- src/commands/scanner.rs
+++
@@ -1,7 +0,0 @@
-use anyhow::Result;
-
-include!(concat!(env!("OUT_DIR"), "/scanner_dispatch.rs"));
-
-pub async fn run_scan(module_name: &str, target: &str) -> Result<()> {
-    dispatch(module_name, target).await
-}
