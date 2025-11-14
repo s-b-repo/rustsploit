512c75ebf1130c2d3ec806972d0f433e7516da96
S.B <30941141+s-b-repo@users.noreply.github.com>
2025-11-14 10:03:32 +0200

    Delete src directory

--- src/modules/scanners/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod sample_scanner;
-pub mod ssdp_msearch;
-pub mod port_scanner;
-pub mod stalkroute_full_traceroute;
-pub mod http_title_scanner;
-pub mod ping_sweep;
-pub mod http_method_scanner;
