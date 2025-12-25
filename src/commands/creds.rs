ebb53590f4e6ebfb73fac05bdedb24d883b34b22
S.B <30941141+s-b-repo@users.noreply.github.com>
2025-12-25 03:31:08 +0200

    Delete src directory

--- src/commands/creds.rs
+++
@@ -1,7 +0,0 @@
-use anyhow::Result;
-
-include!(concat!(env!("OUT_DIR"), "/creds_dispatch.rs"));
-
-pub async fn run_cred_check(module_name: &str, target: &str) -> Result<()> {
-    dispatch(module_name, target).await
-}
